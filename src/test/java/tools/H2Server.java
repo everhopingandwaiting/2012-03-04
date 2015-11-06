7591f3eb58a53d03e278dff386e66aed832208b2
walsh <walsh.zhang@hotmail.com>
2015-11-06 16:23:55 +0800

    remove useless import

--- src/test/java/tools/H2Server.java
+++ src/test/java/tools/H2Server.java
@@ -1,10 +1,6 @@
 package tools;
 
-import org.h2.server.Service;
-import org.h2.server.TcpServer;
 import org.h2.tools.Server;
-
-import java.sql.SQLException;
 
 public class H2Server {
     public static void main(String[] args) throws Exception {
